902d4f2ffc1615e1e619f72d463f2dd4ab09daee
Mary Burford <mburford@paperlesspost.net>
2015-12-30 11:23:33 -0500

    Add everything

---
+++ c.py
@@ -0,0 +1,217 @@
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+import tweepy #https://github.com/tweepy/tweepy
+import csv
+import json
+import random
+import re
+from scipy.misc import imread
+from pprint import pprint
+from time import sleep
+import os
+
+def twitter_api(consumer_key, consumer_secret, access_token, access_token_secret):
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+    return api
+
+
+def search(dictionary, text):
+  print text
+  for k in dictionary.keys():
+    if k in text:
+      print k
+      return dictionary[k]
+      break
+  whoops = random.choice(dictionary.keys())
+  return dictionary[whoops]
+
+
+def creds():
+  with open('creds.json') as data_file:
+        data = json.load(data_file)
+        consumer_key = data['creds'][0]['consumer_key']
+        consumer_secret = data['creds'][0]['consumer_secret']
+        access_token = data['creds'][0]['access_token']
+        access_token_secret = data['creds'][0]['access_token_secret']
+        #return consumer_key, consumer_secret
+        return consumer_key, consumer_secret, access_token, access_token_secret
+
+def listen(consumer_key, consumer_secret, access_key, access_secret,since_id):
+
+
+  #authorize twitter, initialize tweepy
+  auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+  auth.set_access_token(access_key, access_secret)
+  api = tweepy.API(auth)
+  mentions = api.mentions_timeline(count=200,since_id=since_id)
+  return mentions
+
+
+def get_all_tweets(screen_name, consumer_key, consumer_secret, access_key, access_secret):
+  #Twitter only allows access to a users most recent 3240 tweets with this method
+
+  #authorize twitter, initialize tweepy
+  auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+  auth.set_access_token(access_key, access_secret)
+  api = tweepy.API(auth)
+
+  #initialize a list to hold all the tweepy Tweets
+  alltweets = []
+
+  #initialize a list to hold all of the words
+  tweet_text =[]
+
+  #make initial request for most recent tweets (200 is the maximum allowed count)
+  new_tweets = api.user_timeline(screen_name = screen_name,count=200)
+
+  #save most recent tweets
+  alltweets.extend(new_tweets)
+
+  #save the id of the oldest tweet less one
+  oldest = alltweets[-1].id - 1
+
+  #keep grabbing tweets until there are no tweets left to grab
+
+  while len(new_tweets) > 0:
+
+    #all subsiquent requests use the max_id param to prevent duplicates
+    new_tweets = api.user_timeline(screen_name = screen_name,count=200,max_id=oldest)
+
+    #save most recent tweets
+    alltweets.extend(new_tweets)
+
+    #update the id of the oldest tweet less one
+    oldest = alltweets[-1].id - 1
+
+
+  #transform the tweepy tweets into a 2D array that will populate the csv
+  for i in alltweets:
+    tweet = i.text.encode("utf-8").replace('&amp;','&')
+    tweet_text.append(re.sub(r'\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*|\@\w+', '',tweet))
+
+  return tweet_text
+
+
+
+consumer_key, consumer_secret, access_key, access_secret = creds()
+
+masks = {'dog':['dog_shit.png','shitting dog'],'facebook':['facebook_avatar.png','facebook shape'],'twitter':['twitter_mask.png','twitter bird'],
+'bat':['bat.png','bat shape'],'bong':['bong.png','bong'],'horse':['horse.png','horse'],'penis':['penis.png','penis']
+,'woman':['sexy_lady.png','sexualized female'],'weed':['weed_leaf.png','weed leaf'],'comic sans':['Comic Sans MS.ttf','comic sans']
+,'jumpman':['jumpman.png','jumpman'],'kms':['kms.png','kms']}
+seen = []
+log = open('log.txt','r')
+for l in log:
+  last = int(l)
+  seen.append(last+1)
+log.close()
+log = open('log.txt','ab')
+while len(seen) > 0:
+  last_mention = max(seen)
+  mentions =  listen(consumer_key, consumer_secret, access_key, access_secret,last_mention)
+  if len(mentions) < 1:
+    sleep(420)
+    print 'no new mentions, taking a 420 second break'
+  else:
+    for i in mentions:
+      request_id = i.id_str
+      seen.append(int(request_id)+1)
+      log.write(request_id + '\n')
+
+      hashtags = i.entities['hashtags']
+      for h in hashtags:
+        ht = h['text']
+        if 'wordcloud' in ht:
+          sender = i.user.screen_name
+          request_id = i.id_str
+          ats = i.entities['user_mentions']
+          if ats:
+            for a in ats:
+              sn = a['screen_name']
+              if sn != 'computerlab_':
+                user = sn
+              else:
+                user = sender
+          text = i.text
+          text = re.sub(r'\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*|\@\w+|\#\w+', '',text)
+          image,msg_text = search(masks,text)
+          if image == 'facebook_avatar.png':
+            flatten = False
+            background_color = 'white'
+            font = './HelveticaNeue-Bold.ttf'
+          elif image == 'jumpman.png':
+            flatten = False
+            background_color = 'black'
+            font = './HelveticaNeue-Bold.ttf'
+          elif image == 'Comic Sans MS.ttf':
+            flatten = True
+            background_color = 'black'
+            font = './Comic Sans MS.ttf'
+            image = False
+          elif image == 'twitter_mask.png':
+            flatten = False
+            background_color = 'white'
+            font = './HelveticaNeue-Bold.ttf'
+          else:
+            flatten = True
+            background_color = 'black'
+            font = './HelveticaNeue-Bold.ttf'
+
+
+          if sender == 'computerlab_':
+            msg = '@'+user+', look at the '+msg_text+' wordcloud from your tweets that we generated for you!'
+          elif user != sender:
+            msg = '@'+user+', look at the '+msg_text+' wordcloud from your tweets that @'+sender+' generated for you!'
+          else:
+            msg = '@'+user+' thanks for using @computerlab_ wordcloud generator!, cool tweets and '+msg_text+' wordcloud'
+
+
+          # join tweets to a single string
+          words = ' '.join(get_all_tweets(user,consumer_key, consumer_secret, access_key, access_secret))
+
+          # remove URLs, RTs, and twitter handles
+          no_urls_no_tags = " ".join([word for word in words.split()
+                                      if 'http' not in word
+                                          and not word.startswith('@')
+                                          and word.lower()  != 'rt'
+                                          and word.lower() != 'n'
+                                          and word.lower()  != 'w'
+                                          and word.lower()  != 'u'
+                                          and word.lower() != 'got'
+                                      ])
+          if image:
+            twitter_mask = imread('./'+image, flatten=flatten)
+            if flatten is False:
+              image_colors = ImageColorGenerator(twitter_mask)
+            wordcloud = WordCloud(
+                                  font_path=font,
+                                  stopwords=STOPWORDS,
+                                  background_color=background_color,
+                                  width=1800,
+                                  height=1400,
+                                  mask=twitter_mask
+                                 ).generate(no_urls_no_tags)
+          else:
+            wordcloud = WordCloud(
+                      font_path=font,
+                      stopwords=STOPWORDS,
+                      background_color=background_color,
+                      width=1800,
+                      height=1400
+                     ).generate(no_urls_no_tags)
+
+          plt.imshow(wordcloud)
+          plt.axis('off')
+          if flatten is False:
+            plt.imshow(wordcloud.recolor(color_func=image_colors))
+          filename1 = 'resized_image.png'
+          #plt.savefig(filename1, bbox_inches='tight', pad_inches=0)
+          plt.savefig(filename1, dpi=300)
+          api = twitter_api(consumer_key, consumer_secret, access_key, access_secret)
+          api.update_with_media(filename1, status=msg)
+          os.remove(filename1)
+          print 'tweeted '+msg
+          sleep(30)
+
